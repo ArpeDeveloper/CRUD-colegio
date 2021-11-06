d21821eff5967100478cb6b77b3852691423805a
Jesus Perez <jesus.ross@outlook.com>
2021-11-06 13:13:11 -0600

    change view returned in home

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name("home");
+Route::get('/', 'StudentController@index')->name("home");
 Route::resource('students',StudentController::class)->except([
     'create', 'show', 'edit'
 ]);
